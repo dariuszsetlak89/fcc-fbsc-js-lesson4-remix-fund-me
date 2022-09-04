96fcf57355fe9771729b776711ed60d52baf8ccd
KryptoProgramista <kryptoprogramista89@gmail.com>
2022-09-04 10:10:15 +0200

    Contracts and README updated

--- PriceConverter.sol
+++ PriceConverter.sol
@@ -10,18 +10,18 @@ library PriceConverter {
 
     function getPrice() internal view returns (uint256) {
         // ABI
-        // Goerli ETH / USD contract adddress: 	0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e
+        // Rinkeby ETH / USD contract adddress: 0x8A753747A1Fa494EC906cE90E9f37563A8AF630e
         // https://docs.chain.link/docs/ethereum-addresses/ 
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e);
-        // priceFeed give back ETH/USD rate value with 8 digits: 156000000000 == $1560 - 04.09.22
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
+        // priceFeed give back ETH/USD rate value with 8 digits: 155000000000 == $1550 - 05.09.22
         (,int price,,,) = priceFeed.latestRoundData();
-        // ETH/USD rate with 18 digits: 1560_00000000 * 1e10 == 1560_00000000_0000000000 == $1560 - 04.09.22
+        // ETH/USD rate with 18 digits: 1550_00000000 * 1e10 == 1550_00000000_0000000000 == $1550 - 05.09.22
         return uint256(price * 1e10);
     }
 
     function getConversionRate(uint256 ethAmount) internal view returns (uint256) {
         uint256 ethPrice = getPrice();
-        // ETH / USD price: 1560_00000000_0000000000 == $1560 - 04.09.22
+        // ETH / USD price: 1550_00000000_0000000000 == $1550 - 05.09.22
         // 1_00000000_0000000000 ETH
         uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
         // The actual ETH/USD conversion rate, after adjusting the extra 0s.
--- README.md
+++ README.md
@@ -5,4 +5,7 @@
 1. Go to [Remix](https://remix.ethereum.org/)
 2. Paste the code from `FundMe.sol` and `PriceConverter.sol` into new files in Remix
 3. Hit `Compile`
-4. Hit `Deploy`
+4. Hit `Deploy` to Testnet network like Rinkeby or Goerli
+
+### FundMe contract deployed on Rinkeby Testnet network:
+[0xDb622df846aa3b544D162d8C62325f4F067EE89e](https://rinkeby.etherscan.io/address/0xdb622df846aa3b544d162d8c62325f4f067ee89e)
